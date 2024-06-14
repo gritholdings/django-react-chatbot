506e7a22f0ad95e52ebd86d1751705a853eb0be4
Edward Wong <edward@meetgrit.com>
2024-06-14 12:52:38 -0400

    Update package.json to support concurrent

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # django-react-chatbot
+
+## Getting Started
+Before running the application, make sure to install the necessary dependencies. Follow these steps:
+
+1. Open a terminal or command prompt.
+2. Navigate to the project directory.
+
+3. Install the backend dependencies using pip:
+   ```
+   pip install -r requirements.txt
+   ```
+   This command will install all the Python packages listed in the `requirements.txt` file.
+
+4. Navigate to the frontend directory:
+   ```
+   cd frontend
+   ```
+
+5. Install the frontend dependencies using npm:
+   ```
+   npm install
+   ```
+   This command will install all the JavaScript packages required by the frontend.
+
+Once the dependencies are installed, you can run the application:
+
+6. Navigate back to the project root directory:
+   ```
+   cd ..
+   ```
+
+7. Run the following command to start the application:
+   ```
+   npm run start
+   ```
+
+This command will execute `python manage.py run server` to start the backend server and `npm start` to start the frontend development server concurrently.
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, CardColumns, Card, CardBody, CardText } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="App-body">
+            <div>Model:</div>
+            <CardColumns style={{width: '24rem'}}>
+                <Card>
+                    <CardBody>
+                        <CardText>
+                            This card has supporting text below as an additional content 1.
+                        </CardText>
+                    </CardBody>
+                </Card>
+                <Card>
+                    <CardBody>
+                        <CardText>
+                            <form>
+                                <div className="mb-3">
+                                    <textarea className="form-control" id="inputTextarea" rows="1" placeholder="Message Chatbot..."></textarea>
+                                </div>
+                                <button type="submit" className="btn btn-primary">Submit</button>
+                            </form>
+                        </CardText>
+                    </CardBody>
+                </Card>
+            </CardColumns>
+        </div>
     </div>
-  );
+    );
 }
 
 export default App;
