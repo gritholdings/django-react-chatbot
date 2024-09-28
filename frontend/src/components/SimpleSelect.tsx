7187ba59b7ceed9f4fff5b49a7c370ed91a1a949
Edward Wong <edward@meetgrit.com>
2024-09-28 00:16:53 -0400

    Remove SimpleSelect.css

--- frontend/src/components/SimpleSelect.tsx
+++ frontend/src/components/SimpleSelect.tsx
@@ -9,7 +9,7 @@ const SimpleSelect = () => (
       className="inline-flex items-center justify-center rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px] bg-white text-violet11 shadow-[0_2px_10px] shadow-black/10 hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-violet9 outline-none"
       aria-label="Food"
     >
-      <Select.Value placeholder="Select a fruit…" />
+      <Select.Value placeholder="Select Model" />
       <Select.Icon className="text-violet11">
         <ChevronDownIcon />
       </Select.Icon>
